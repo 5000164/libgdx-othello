ccc2e86a560b84615344f8f4494e16b412eaad6a
Hiroshi Sugawara <5000164@users.noreply.github.com>
2017-03-02 02:24:23 +0900

    クリックしたら反応するボタンを追加

--- core/src/main/scala/jp/_5000164/libgdx_othello/OthelloGame.scala
+++ core/src/main/scala/jp/_5000164/libgdx_othello/OthelloGame.scala
@@ -3,6 +3,11 @@ package jp._5000164.libgdx_othello
 import com.badlogic.gdx.graphics.g2d.{BitmapFont, SpriteBatch}
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer
 import com.badlogic.gdx.graphics.{GL20, OrthographicCamera}
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton.TextButtonStyle
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener
+import com.badlogic.gdx.scenes.scene2d.{InputEvent, Stage}
+import com.badlogic.gdx.utils.viewport.ScreenViewport
 import com.badlogic.gdx.{Game, Gdx, Input, Screen}
 
 class OthelloGame extends Game {
@@ -63,6 +68,20 @@ class PlayScreen(game: OthelloGame) extends Screen {
   camera.setToOrtho(false, 800, 480)
   lazy val shapeRenderer = new ShapeRenderer()
 
+  val stage = new Stage(new ScreenViewport())
+  Gdx.input.setInputProcessor(stage)
+  val tbs = new TextButtonStyle()
+  tbs.font = game.font
+  val tb = new TextButton("Text on the Button", tbs)
+  tb.setPosition(300f, 300f)
+  tb.getLabel.setFontScale(2)
+  tb.addListener(new ClickListener {
+    override def clicked(event: InputEvent, x: Float, y: Float): Unit = {
+      println("clicked")
+    }
+  })
+  stage.addActor(tb)
+
   override def render(delta: Float) {
     Gdx.gl.glClearColor(0, 0, 0, 1)
     Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
@@ -83,6 +102,9 @@ class PlayScreen(game: OthelloGame) extends Screen {
     game.batch.begin()
     game.font.draw(game.batch, "Play", 100, 100)
     game.batch.end()
+
+    stage.act(delta)
+    stage.draw()
   }
 
   override def resize(width: Int, height: Int) {}
