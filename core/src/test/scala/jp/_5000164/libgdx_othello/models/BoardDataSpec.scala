6a762922c49656a2130ae63872e33e23c3e5bbf9
Hiroshi Sugawara <5000164@users.noreply.github.com>
2017-03-25 23:25:48 +0900

    盤面の更新方法についてのテストを追加

--- core/src/test/scala/jp/_5000164/libgdx_othello/models/BoardDataSpec.scala
+++ core/src/test/scala/jp/_5000164/libgdx_othello/models/BoardDataSpec.scala
@@ -10,5 +10,11 @@ class BoardDataSpec extends FreeSpec {
       assert("scala.collection.immutable.Map$Map1" == boardData.data(1).getClass.getTypeName)
       assert("jp._5000164.libgdx_othello.models.Empty$" == boardData.data(1)(1).getClass.getTypeName)
     }
+
+    "盤面の情報を元に新しい盤面を作る" in {
+      val boardData: BoardData = BoardData(Map(1 -> Map(1 -> Empty)))
+      val newBoardData = BoardData(boardData.data.updated(1, Map(1 -> Black)))
+      assert(newBoardData.data(1)(1) == Black)
+    }
   }
 }
